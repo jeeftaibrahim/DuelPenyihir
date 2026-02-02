8b34cbb77d2c807e7973c482906293f3880c56ae
jeeftaibrahim <jeeftaif@gmail.com>
2026-02-02 11:16:26 +0700

    Menambah Class Wizard, dua object Wizard, dan simulasi battle

--- Program.cs
+++ Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Wizard wizardA = new Wizard("Juye Horis", 20);
+Wizard wizardB = new Wizard("Hegoes MgOes", 30);
+
+Console.WriteLine("Permainan dimulai......\n");
+wizardA.ShowStats();
+wizardB.ShowStats();
+
+wizardA.Attack(wizardB);
+wizardB.Attack(wizardA);
+wizardA.Attack(wizardB);
+
+Console.WriteLine("Permainan berakhir....\n");
+wizardA.ShowStats();
+wizardB.ShowStats();
+
+public class Wizard
+
+{
+    //deklarasi field
+    public string Name;
+    public int Energy;
+    public int Damage;
+
+    //deklarasi constructor
+    public Wizard(string name,int damage)
+    {
+        Name = name;
+        Energy = 100;
+        Damage = damage;
+
+    }
+
+    public void ShowStats()
+    {
+        Console.WriteLine("statistik wizard");
+        Console.WriteLine($"Nama: {Name}, Energi: {Energy} \n");
+    }
+    public void Attack(Wizard wizardLawanObj)
+    {
+        //mengurangi energi wizardLawanObj sebesar damage
+        wizardLawanObj.Energy -= Damage;
+        Console.WriteLine($"{Name} menyerang {wizardLawanObj.Name}");
+        Console.WriteLine($"sisa energi {wizardLawanObj.Name} adalah {wizardLawanObj.Energy}");
+    }
+}
