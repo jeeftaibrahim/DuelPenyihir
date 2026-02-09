01b52571bd5d7653837c1659864711941c801955
jeeftaibrahim <jeeftaif@gmail.com>
2026-02-09 10:54:36 +0700

    akhir permainan

--- Program.cs
+++ Program.cs
@@ -5,14 +5,57 @@ Console.WriteLine("Permainan dimulai......\n");
 wizardA.ShowStats();
 wizardB.ShowStats();
 
-wizardA.Attack(wizardB);
-wizardB.Attack(wizardA);
-wizardA.Heal();
-wizardA.Attack(wizardB);
+//wizardA.Attack(wizardB);
+//wizardB.Attack(wizardA);
+//wizardA.Heal();
+//wizardA.Attack(wizardB);
+string pilihan;
+while (wizardA.Energy > 0 && wizardB.Energy > 0)
+{
+    Console.WriteLine($"1. {wizardA.Name} menyerang {wizardB.Name}");
+    Console.WriteLine($"2. {wizardB.Name} menyerang {wizardA.Name}");
+    Console.WriteLine($"3. {wizardA.Name} melakukan heal");
+    Console.WriteLine($"4. {wizardB.Name} melakukan heal");
+
+    Console.Write("\nmasukan pilihanmu (1/2/3/4): ");
+    pilihan = Console.ReadLine();
+
+    if ( pilihan == "1")
+    {
+        wizardA.Attack(wizardB);
+    }
+    else if (pilihan == "2")
+    {
+        wizardB.Attack(wizardA);
+    }
+    else if ( pilihan == "3")
+    {
+        wizardA.Heal();
+    }
+    else if ( pilihan == "4")
+
+    {
+        wizardB.Heal();
+    }
+    else
+    {
+        Console.WriteLine("pilihan tidak valid");
+    }
+}
 
 Console.WriteLine("Permainan berakhir....\n");
+Console.WriteLine("statistik akhir");
 wizardA.ShowStats();
 wizardB.ShowStats();
+
+if (wizardA.Energy < wizardB.Energy)
+{
+    Console.WriteLine($"{wizardB.Name} memenangkan duel");
+}
+else 
+{
+    Console.WriteLine($"{wizardB.Name} memenangkan duel");
+}
 
 public class Wizard
 
@@ -26,7 +69,7 @@ public class Wizard
     public Wizard(string name,int damage)
     {
         Name = name;
-        Energy = 100;
+        Energy = 50;
         Damage = damage;
 
     }
@@ -46,7 +89,7 @@ public class Wizard
     public void Heal()
     {
         Energy += 5;
-        if (Energy <= 100)
+        if (Energy <= 50)
         {
            
             Console.WriteLine($"{Name} melakukan heal meningkatkan menjadi{Energy}" );
